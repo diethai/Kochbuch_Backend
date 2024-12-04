b8d251768c480be3b433f9bade6607940f389710
maria.hanebutt <maria.hanebutt@gwdg.de>
2024-12-04 09:47:18 +0100

    -Autor klasse und AutorRepository kommentiert

--- src/main/java/de/gwdg/kochbuch_backend/model/dao/AutorRepository.java
+++ src/main/java/de/gwdg/kochbuch_backend/model/dao/AutorRepository.java
@@ -1,12 +1,11 @@
-package de.gwdg.kochbuch_backend.model.dao;
+package de.gwdg.kochbuch_backend.model.dao; /* Hier wird der Package-Name angegeben, in dem sich die Datei befindet. */
 
-import de.gwdg.kochbuch_backend.model.dto.Autor;
-import org.springframework.data.repository.CrudRepository;
+import de.gwdg.kochbuch_backend.model.dto.Autor; /* Hier wird der Import des Autor-DTOs durchgeführt. */
+import org.springframework.data.repository.CrudRepository; /* Hier wird der Import der CrudRepository-Klasse durchgeführt. */
 
-import java.util.Optional;
+import java.util.Optional; /* Hier wird der Import der Optional-Klasse durchgeführt. */
 
-public interface AutorRepository extends CrudRepository<Autor, Long> {
+public interface AutorRepository extends CrudRepository<Autor, Long> { /* Hier wird die AutorRepository-Klasse definiert, die die CrudRepository-Klasse erweitert. */
 
-    Optional<Autor> findById(Long id);
-
+    Optional<Autor> findById(Long id);                                 /* Hier wird die Methode findById definiert, die einen Autor mit der spezifischen ID zurückgibt. */
 }
--- src/main/java/de/gwdg/kochbuch_backend/model/dto/Autor.java
+++ src/main/java/de/gwdg/kochbuch_backend/model/dto/Autor.java
@@ -1,27 +1,29 @@
-package de.gwdg.kochbuch_backend.model.dto;
+package de.gwdg.kochbuch_backend.model.dto; /* Package-Name der Klasse */
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.Entity; /* Import der Entity-Klasse */
+import jakarta.persistence.Id; /* Import der Id-Klasse */
+import jakarta.persistence.GeneratedValue; /* Import der GeneratedValue-Klasse */
+import jakarta.persistence.GenerationType; /* Import der GenerationType-Klasse */
+import jakarta.persistence.*; /* Import der Persistenz-Annotatoren */
+import lombok.AllArgsConstructor; /* Import der Lombok-Annotatoren */
+import lombok.Getter; /* Import der Getter-Annotatoren */
+import lombok.NoArgsConstructor; /* Import der NoArgsConstructor-Annotatoren */
+import lombok.Setter; /* Import der Setter-Annotatoren */
 
-@Entity
-@Table (name = "autoren") // specify the name of the table
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class Autor {
+@Entity                      /* Die Klasse ist eine Persistenz-Klasse */
+@Table (name = "autoren")    /* Die Tabelle wird als "autoren" benannt */
+@Getter                      /* Die Getter-Methode wird generiert */
+@Setter                      /* Die Setter-Methode wird generiert */
+@NoArgsConstructor           /* Der Konstruktor ohne Parameter wird generiert */
+@AllArgsConstructor          /* Der Konstruktor mit allen Feldern wird generiert */
+public class Autor {         /* Die Klasse "Autor" wird definiert */
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    @Id /* Die ID ist eindeutig */
+    @GeneratedValue(strategy = GenerationType.IDENTITY) /* Die ID wird automatisch generiert */
+    private long id; /* Die ID ist eine Ganzzahl */
 
-    private String AutorName;
+    private String AutorName; /* Der Name des Autors ist eine Zeichenkette */
+
+}
 
 }
